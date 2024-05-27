7b52ffee8e9ba234eeb2af6cfea01499c62d8d45
Lokankara <Lokankara@users.noreply.github.com>
2024-05-27 20:03:26 +0000

    deploy: dddd4adae1f4b7499b88d4848e5e594fd981b333

--- java17.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set JAVA_HOME=c:\Users\tmp\.jdks\corretto-17.0.10
-set Path=%JAVA_HOME%\bin;%Path%
-echo Java 17 activated.
--- src/test/java/com/luma/runner/TestData.java
+++
@@ -1,5 +0,0 @@
-package com.luma.runner;
-
-public class TestData {
-    public static final String MAGENTO_BASE_URL = "https://magento.softwaretestingboard.com";
-}
